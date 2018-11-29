ef7aaea0a6971d53b200f173a614441635713e72
yuuki <devel.yuuki@gmail.com>
2018-11-29 19:06:33 +0900

    Merge pull request #15 from s-takizawa-0216/ルーティング設定2
    ルーティング設定2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'merkaries#index'
+  resources :merkaries
+  resources :iems
+  resources :uers
 end
--- db/migrate/20181127073619_create_brands_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateBrandsCategories < ActiveRecord::Migration[5.2]
-  def change
-    create_table :brands_categories do |t|
-
-      t.timestamps
-    end
-  end
-end
